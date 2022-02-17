c86d5768f47042241d19fc261a8e8103ae4798f5
Justin Lynch <jusjoker@gmail.com>
2022-02-17 17:21:32 -0500

    did my js hw

---
+++ main.js
@@ -0,0 +1,9 @@
+let firstName = 'Justin ';
+let lastName = 'Lynch';
+let age = 19;
+
+alert(`My name is ${firstName + lastName}.`);
+alert(`I am ${age} years old.`);
+
+console.log("I am having fun learning different coding languages in this course.")
+
